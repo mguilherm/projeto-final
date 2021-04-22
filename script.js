3a544e3bf50658afbf211146067ed3765ac4bee6
Marcos Guilherme Silva <71297313+mguilherm@users.noreply.github.com>
2021-04-21 22:23:16 -0300

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function menuIcon(x) {
+    x.classList.toggle("change");
+}
+
+function openNav() {
+    document.getElementById("menuOptions").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("menuOptions").style.width = "0%";
+}
